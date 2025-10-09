422dd2392aa6dab904c4c623a68b799daa6b7c77
Cristóbal Rey <cristobal.rey.campos@gmail.com>
2025-10-09 15:55:43 -0300

    Mejorando clases

--- clases/Empleado.py
+++ clases/Empleado.py
@@ -1,33 +1,37 @@
 from clases.modelos.Persona import Persona
 from RegistroHorario import RegistroHorario
-from datetime import datetime
-
-fecha_actual = datetime.now().strftime("%d-%m-%Y")
+import mysql.connector
+from datetime import date
 
 class Empleado(Persona):
     def __init__(self, nombre='', direccion='', telefono='', email='', inicio_contrato = None, salario = 0, proyectos = None ):
         #CAMBIAR EL TIPO DE DATO DE PROYECTOS PARA CUANDO HAYA UNO
         super().__init__(nombre, direccion, telefono, email)
-        self.inicio_contrato = inicio_contrato
-        self.salario = salario
+        self.__inicio_contrato = inicio_contrato or date.today()
+        self.__salario = salario
         if proyectos == None:
-            self.proyectos = []
-        self.proyectos = proyectos
+            self.__proyectos = []
+        self.__proyectos = proyectos
     
-    @classmethod
-    def crearEmpleado(cls):
-        nombre = input('Ingrese el nombre del empleado: ')
-        direccion = input('Ingrese direccion del empleado: ')
-        telefono = input('Ingrese telefono del empleado: ')
-        email = input('Ingrese el email del empleado: ')
-        inicio_contrato = fecha_actual
-        salario = int(input('Ingrese el salario del empleado: '))
-        proyectos = '' #implementar mas adelante
-        return cls(nombre, direccion, telefono, email, inicio_contrato, salario, proyectos)
+    @property
+    def inicio_contrato(self):
+        return self.__inicio_contrato
     
+    @property
+    def salario (self):
+        return self.__salario
+    
+    @salario.setter
+    def salario(self, salario):
+        if salario < 0:
+            raise ValueError('\nEl salario no puede ser negativo\n')
+        self.__salario = salario
+        
+                
+            
     def crearRegistro(self):
         horas_trabajadas = int(input('Ingrese sus horas trabajadas: '))
-        fecha = fecha_actual
+        fecha = ''
         descripcion_tarea = input('Ingrese la descripción de la tarea [200 caracteres máximo]: ')
         registro_final = RegistroHorario(horas_trabajadas, fecha, descripcion_tarea)
         
